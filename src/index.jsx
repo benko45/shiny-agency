13e3dc093a5c35137d49dbce4f269b552d20a8a9
Benoît Vicente <benoit.vicente@hotmail.fr>
2023-12-29 19:26:01 +0100

    renommage ThemeProvider.jsx => ContentProvider.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
-import { ThemeProvider } from './utils/context/ThemeProvider.jsx'
+import { ThemeProvider } from './utils/context/ContextProvider.jsx'
 
 import Home from './pages/Home/Home.jsx'
 import Survey from './pages/Survey/Survey.jsx'
